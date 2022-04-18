1fd3616de0d2369840570b40747ee8f78f1985b5
ClassyMan <yardy_14@hotmail.com>
2022-04-17 18:19:42 -0700

    Add files via upload

--- caravan_2_arrows_proper/keymaps/coding/keymap.c
+++ caravan_2_arrows_proper/keymaps/coding/keymap.c
@@ -15,7 +15,8 @@ enum custom_keycodes {
   JIRAQUOTE,
   JIRACODE,
   SCREENSHOT,
-  LOCALHOST,
+  LOCALHOST, 
+  SYSPRINT,
   ALL_ENTITIES
 };
 
@@ -68,6 +69,24 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         SEND_STRING("localhost:8079/");
       }
       break;
+    case SYSPRINT:
+      if (record->event.pressed) {
+        register_code(KC_LCTRL);
+        tap_code(KC_C);
+        clear_keyboard();
+        SEND_STRING("System.out.println();");
+        tap_code(KC_LEFT);
+        tap_code(KC_LEFT);
+        SEND_STRING("\"");
+        register_code(KC_LCTRL);
+        tap_code(KC_V);
+        clear_keyboard();
+        SEND_STRING(": \" + ");
+        register_code(KC_LCTRL);
+        tap_code(KC_V);
+        clear_keyboard();        
+      }
+      break;
     case ALL_ENTITIES:
       if (record->event.pressed) {
         SEND_STRING("*:*");
@@ -104,7 +123,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LGUI,        JIRAQUOTE,      KC_LGUI,        KC_TRANSPARENT, RESET,          KC_TRANSPARENT, KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,         KC_F12,
     KC_INSERT,      KC_TRANSPARENT, SCHEDULESTATUS, KC_TRANSPARENT, FORLOOP,        WHILELOOP,      KC_F4,          KC_F5,          KC_F6,          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TRANSPARENT, KC_TRANSPARENT, SCREENSHOT,     JIRACODE,       KC_TRANSPARENT, KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,          KC_MUTE,        KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, MO(_FN3),       KC_TRANSPARENT,                 KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
+    KC_TRANSPARENT, MO(_FN3),       KC_TRANSPARENT,                 KC_TRANSPARENT, SYSPRINT,                                       KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
   ),
 
   [_GAMING] = LAYOUT(
